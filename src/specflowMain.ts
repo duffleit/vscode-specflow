e3870fee62de34c45bda6acca597b1a2f2b8fdd9
duffleit <davidleitner@live.at>
2015-11-30 00:43:09 +0100

    Upaded "About SpecFlow" command.

--- src/specflowMain.ts
+++ src/specflowMain.ts
@@ -10,7 +10,7 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.languages.registerCompletionItemProvider(
 		SPECFLOWMODE, new SpecflowCompletionItemProvider(), '.'));
 
-	var disposable = vscode.commands.registerCommand('about Specflow', () => {
+	var disposable = vscode.commands.registerCommand('extension.aboutSpecflow', () => {
 		vscode.window.showInformationMessage('If you want to get more information about specflow visit us on specflow.org');
 	});
 		
