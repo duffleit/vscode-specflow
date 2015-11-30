cb8bbabd634cd7a22876641850602450537e49bc
duffleit <davidleitner@live.at>
2015-11-30 01:02:33 +0100

    added check, if cucumber extension is installed

--- src/specflowMain.ts
+++ src/specflowMain.ts
@@ -5,13 +5,21 @@ import {SPECFLOWMODE } from './specflowMode';
 
 export function activate(context: vscode.ExtensionContext) {
 
+	//check if cucumber extension is installed
+	var cucumber = vscode.extensions.getExtension("stevejpurves.cucumber");
+	if(cucumber === undefined || cucumber === null){
+		vscode.window.showErrorMessage('To Use Specflow Integration please install the Visual Studio Code Cucumber extension.')
+		vscode.commands.registerCommand('extension.aboutSpecflow', () => {});
+		return;
+	}
+
 	console.log('extension "specflow" is active'); 
 
 	context.subscriptions.push(vscode.languages.registerCompletionItemProvider(
 		SPECFLOWMODE, new SpecflowCompletionItemProvider(), '.'));
-
+		
 	var disposable = vscode.commands.registerCommand('extension.aboutSpecflow', () => {
-		vscode.window.showInformationMessage('If you want to get more information about specflow visit us on specflow.org');
+		vscode.window.showInformationMessage('If you want to get more information about specflow visit us on specflow.org');	
 	});
 		
 	context.subscriptions.push(disposable);
